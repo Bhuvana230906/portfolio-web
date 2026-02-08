b2077c91ccb303f6f00a8394e40269343cff4004
Bhuvana230906 <bhuvanagopi2309@gmail.com>
2026-02-08 21:36:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMessage() {
+    alert("Thank you for visiting my portfolio!");
+}
+
+function showSection(id) {
+    const sections = document.querySelectorAll("section");
+
+    sections.forEach(section => {
+        section.style.display = "none";
+    });
+
+    document.getElementById(id).style.display = "block";
+}
